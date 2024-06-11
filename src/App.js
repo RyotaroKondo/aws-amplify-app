920fe4b6f1f66b578c5aa2d35e9c3de9c73962c9
r-kondo <ry.script@gmail.com>
2024-06-12 02:59:02 +0900

    style-dictionaryを追加ã

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import './variables.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello React app with AWS</h2>
+        <h2 className="text-color-gray-light">Hello React app with <span class="color-base-green">AWS</span></h2>
+        <div className="color-base-red">Test Style-Dictionary</div>
+        <div className="color-text-link">HogehogeFugafuga</div>
       </header>
     </div>
   );
