7321194835700bdf03deeacb0b9e97051c22ec5a
近藤遼汰朗 <r-kondo@fabrica-com.co.jp>
2024-06-24 19:12:17 +0900

    token test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h2 className="text-color-gray-light">Hello React app with <span class="color-base-green">AWS</span></h2>
         <div className="color-base-red">Test Style-Dictionary</div>
         <div className="color-text-link">HogehogeFugafuga</div>
+        <button className='button-danger'>削除</button>
       </header>
     </div>
   );
